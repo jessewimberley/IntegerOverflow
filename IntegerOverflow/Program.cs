641cbd2d9dcce65d4b15204d32fce7dee8961b17
Jesse Wimberley <jesse.wimberley@gmail.com>
2020-12-13 09:52:59 -0800

    Initial Commit

---
+++ IntegerOverflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntegerOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.MaxValue;
+            Console.WriteLine("Maximum value for a signed integer: " + i);
+            Console.WriteLine("0" + Convert.ToString(i, 2));
+            Console.ReadLine();
+            Console.WriteLine();
+            i += 1;
+            Console.WriteLine("Maximum value +1: " + i);
+            Console.WriteLine(Convert.ToString(i, 2));
+            Console.ReadLine();
+            Console.WriteLine();
+            i += 1;
+            Console.WriteLine("Max vaule + 2: " + i);
+            Console.WriteLine(Convert.ToString(i, 2));
+            Console.ReadLine();
+        }
+
+    }
+}
